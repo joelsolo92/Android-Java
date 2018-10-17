7bb094052ea6583851120d4c56e6b1985918b589
Eyoel Demissie <eyoelsolomon@gmail.com>
2018-10-17 17:39:24 -0400

    added buttons, EditText, Label to Activity

--- Palindrome/app/src/main/java/com/solomon/eyoel/palindrome/PalindromeActivity.java
+++ Palindrome/app/src/main/java/com/solomon/eyoel/palindrome/PalindromeActivity.java
@@ -10,6 +10,5 @@ public class PalindromeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palindrome);
 
-        System.out.println();
     }
 }
