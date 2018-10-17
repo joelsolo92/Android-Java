bb973297deb323ea90ac680c9cb95b53ca09c39a
Eyoel Demissie <eyoelsolomon@gmail.com>
2018-10-17 18:47:09 -0400

    added remaining event handlers

--- Palindrome/app/src/main/java/com/solomon/eyoel/palindrome/PalindromeActivity.java
+++ Palindrome/app/src/main/java/com/solomon/eyoel/palindrome/PalindromeActivity.java
@@ -1,16 +1,22 @@
 package com.solomon.eyoel.palindrome;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Stack;
 
 public class PalindromeActivity extends AppCompatActivity {
 
-    Button btnSubmit;
-    Button btnClear;
-    Button btnQuit;
+    Button mSubmit;
+    Button mClear;
+    Button mQuit;
+    EditText mValue;
 
 
     @Override
@@ -18,26 +24,48 @@ public class PalindromeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palindrome);
 
-        btnSubmit = findViewById(R.id.btn_submit);
-        btnClear = findViewById(R.id.btn_clear);
-        btnQuit = findViewById(R.id.btn_quit);
+        mSubmit = findViewById(R.id.btn_submit);
+        mClear = findViewById(R.id.btn_clear);
+        mQuit = findViewById(R.id.btn_quit);
+        mValue = findViewById(R.id.edit_user_entry);
 
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+        mSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getValues();
             }
         });
 
+        mClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValue.setText("");
+            }
+        });
+
+        mQuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
+
+
     }
 
     public void getValues() {
 
         String text = "";
-        text = btnSubmit.getText().toString();
+        text = mValue.getText().toString();
 
-        if (text == ""){
+        if (text.isEmpty()) {
             Toast.makeText(this, "Fieid can not be empty.", Toast.LENGTH_SHORT).show();
+            Log.e("this tag", "error");
+        } else {
+
+            String newText = text.replaceAll("[^a-zA-Z0-9]", "");
+            Log.e("tag", newText);
+
         }
     }
 }
